95c351cf5de6d8f18681ac6bd20fa58ac6ab822f
sakthi <sakthipalanisamy7@gmail.com>
2025-04-03 21:27:42 +0530

    error resolved

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import Portfolio from './App.jsx'
+import Portfolio from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Portfolio />
-  </StrictMode>,
-)
+  </StrictMode>
+);
